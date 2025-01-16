8a6c1f1fa14aa9ba7e82ba8f8cbbc265da169817
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-01-16 22:53:54 +0000

    app config & routing setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import {Component} from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {RouterOutlet} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  template: `
-    <h1>Default</h1>
-  `,
-  styleUrls: ['./app.component.css'],
+    selector: 'app-root',
+    imports: [
+        RouterOutlet,
+        MatToolbar,
+        MatIcon,
+        MatIconButton
+    ],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'default';
+    title = 'porygondb';
 }
