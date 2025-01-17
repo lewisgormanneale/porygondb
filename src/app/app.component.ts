cef6a6114c8c239bc333efc8235787c81c6b5343
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-01-17 12:05:16 +0000

    github link in header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
-import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
+import {MatIcon, MatIconRegistry} from "@angular/material/icon";
+import {MatIconAnchor, MatIconButton} from "@angular/material/button";
 import {RouterOutlet} from "@angular/router";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
     selector: 'app-root',
@@ -10,11 +11,20 @@ import {RouterOutlet} from "@angular/router";
         RouterOutlet,
         MatToolbar,
         MatIcon,
-        MatIconButton
+        MatIconButton,
+        MatIconAnchor
     ],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
     title = 'porygondb';
+
+    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistry.addSvgIcon(
+            'github',
+            sanitizer.bypassSecurityTrustResourceUrl('assets/icons/github.svg')
+        );
+    }
+
 }
