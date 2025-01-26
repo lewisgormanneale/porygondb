98f115b9fa9001f53c11845bf5acb43bb8a2c39a
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-01-26 14:44:43 +0000

    progress bar while fetching pokedex

--- src/app/pages/pokedex/pokedex.component.ts
+++ src/app/pages/pokedex/pokedex.component.ts
@@ -5,23 +5,32 @@ import { AppState } from "../../store/app.state";
 import { loadPokedex } from "../../store/actions/pokedex.actions";
 import { PokedexResult } from "../../models/pokedex.model";
 import { AsyncPipe } from "@angular/common";
-import { MatPaginator, PageEvent } from "@angular/material/paginator";
+import { MatPaginatorModule, PageEvent } from "@angular/material/paginator";
 import { MatCardModule } from "@angular/material/card";
-import { MatButton } from "@angular/material/button";
+import { MatButtonModule } from "@angular/material/button";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
 
 @Component({
   selector: "app-pokedex",
-  imports: [AsyncPipe, MatPaginator, MatCardModule, MatButton],
+  imports: [
+    AsyncPipe,
+    MatPaginatorModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+  ],
   templateUrl: "pokedex.component.html",
   styleUrl: "pokedex.component.scss",
 })
 export class PokedexComponent implements OnInit {
   pokedex$: Observable<PokedexResult[]>;
   totalCount$: Observable<number>;
+  loading$: Observable<boolean>;
 
   constructor(private store: Store<AppState>) {
     this.pokedex$ = this.store.select((state) => state.pokedex.pokedex);
     this.totalCount$ = this.store.select((state) => state.pokedex.totalCount);
+    this.loading$ = this.store.select((state) => state.pokedex.loading);
   }
 
   ngOnInit(): void {
