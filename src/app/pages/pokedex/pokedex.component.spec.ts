deeff62d03dbf20207beddf907fdfe2501224395
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-02-08 17:06:14 +0000

    set up repo with placeholder test

---
+++ src/app/pages/pokedex/pokedex.component.spec.ts
@@ -0,0 +1,41 @@
+import { PokedexComponent } from "./pokedex.component";
+import { HarnessLoader } from "@angular/cdk/testing";
+import { TestbedHarnessEnvironment } from "@angular/cdk/testing/testbed";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatPaginatorHarness } from "@angular/material/paginator/testing";
+import { provideMockStore } from "@ngrx/store/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatPaginatorModule } from "@angular/material/paginator";
+
+describe("PokédexComponent", () => {
+  let fixture: ComponentFixture<PokedexComponent>;
+  let loader: HarnessLoader;
+  let component: PokedexComponent;
+  const initialState = {
+    loading: false,
+    pokedex: [
+      {
+        id: 1,
+        name: "bulbasaur",
+        url: "https://pokeapi.co/api/v2/pokemon/1/",
+      },
+    ],
+    totalCount: 100,
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PokedexComponent, MatPaginatorModule, NoopAnimationsModule],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PokedexComponent);
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component = fixture.componentInstance;
+  });
+
+  it("The page should load", async () => {
+    expect(component).toBeTruthy();
+  });
+});
