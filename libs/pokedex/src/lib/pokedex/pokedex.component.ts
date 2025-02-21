b30e2b1e12279bb3aff2feb31bf1bf973f0ac29e
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-02-21 14:53:28 +0000

    creating pokedex lib

---
+++ libs/pokedex/src/lib/pokedex/pokedex.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-pokedex',
+  imports: [CommonModule],
+  templateUrl: './pokedex.component.html',
+  styleUrl: './pokedex.component.css',
+})
+export class PokedexComponent {}
