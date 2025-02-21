4a52ee4f9cd9d004710c1205e29f427153608916
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-02-21 18:07:07 +0000

    fix duplicate baseConfig for eslint

--- libs/shared/ui/eslint.config.mjs
+++ libs/shared/ui/eslint.config.mjs
@@ -1,6 +1,5 @@
 import baseConfig from '../../../eslint.base.config.mjs';
 import nx from '@nx/eslint-plugin';
-import baseConfig from '../../../eslint.config.mjs';
 
 export default [
   ...baseConfig,
