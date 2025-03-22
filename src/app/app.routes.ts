ae252391517940835dfd256203ac085f52885fc4
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2025-03-22 14:16:53 +0000

    styling cleanup, skeleton styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { PokemonComponent } from "./features/pokemon/pages/pokemon/pokemon.compo
 import { PageNotFoundComponent } from "./shared/components/page-not-found/page-not-found.component";
 
 export const routes: Routes = [
-  { path: "", redirectTo: "/pokedex/scarlet-violet", pathMatch: "full" },
+  { path: "", redirectTo: "/pokedex/red-blue", pathMatch: "full" },
   { path: "pokedex/:versionGroupName", component: PokedexComponent },
   { path: "pokemon/:name", component: PokemonComponent },
   { path: "**", component: PageNotFoundComponent },
